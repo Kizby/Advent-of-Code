29452b7c99c0ee3bd0fb47bc2ea9801331eefed2
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2021-11-27 08:26:13 -0500

    Turn off warnings for httplib

--- Utility/utility.h
+++ Utility/utility.h
@@ -3,7 +3,9 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
 #define CPPHTTPLIB_OPENSSL_SUPPORT
+#pragma warning(push, 0)
 #include "httplib.h"
+#pragma warning(pop)
 
 #include <fstream>
 #include <iostream>
